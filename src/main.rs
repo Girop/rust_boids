d4766bb38b93343104e46567ecb90066cc3f9ca6
unknown <pigor2400@gmail.com>
2022-07-02 02:40:40 +0200

    startup camera added

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
+#![allow(unused)]
 use bevy::prelude::*;
+use rand::Rng;
+
+const BOID_POPULATION: i32 = 30;
+const BOID_SORITE: &str = "triangle.jpg";
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
 }
 
+struct Boid {
+    x: f32,
+    y: f32,
+}
+
+impl Boid {
+    pub fn new() -> Self {
+        let mut rng = rand::thread_rng();
+        let x = rng.gen::<f32>();
+        let y = rng.gen::<f32>();
+        Boid { x: x, y: y }
+    }
+
+    pub fn live(&mut self) {
+        let mut rng = rand::thread_rng();
+        let x = rng.gen::<f32>();
+        let y = rng.gen::<f32>();
+        self.x += x;
+        self.y += y;
+    }
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
