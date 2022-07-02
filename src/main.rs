dd0570f67abcad417a1c9f75dcfe73a04512f16c
unknown <pigor2400@gmail.com>
2022-07-02 23:56:40 +0200

    unreadable error on triangle creation attempt

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,55 @@
 #![allow(unused)]
-use bevy::prelude::*;
-use rand::Rng;
+use bevy::{
+    prelude::*,
+    render::mesh::{Indices, PrimitiveTopology},
+    sprite::MaterialMesh2dBundle,
+};
 
-const BOID_POPULATION: i32 = 30;
-const BOID_SORITE: &str = "triangle.jpg";
+// const BOID_POPULATION: i32 = 30;
+
+// Entities
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor {
+            title: "Boiiiiids".to_string(),
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_startup_system(spawn_boid)
         .run();
 }
 
-struct Boid {
-    x: f32,
-    y: f32,
-}
-
-impl Boid {
-    pub fn new() -> Self {
-        let mut rng = rand::thread_rng();
-        let x = rng.gen::<f32>();
-        let y = rng.gen::<f32>();
-        Boid { x: x, y: y }
-    }
-
-    pub fn live(&mut self) {
-        let mut rng = rand::thread_rng();
-        let x = rng.gen::<f32>();
-        let y = rng.gen::<f32>();
-        self.x += x;
-        self.y += y;
-    }
-}
-
 fn setup(mut commands: Commands) {
+    // Create camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn spawn_boid(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn_bundle(MaterialMesh2dBundle {
+        mesh: meshes.add(triangle_mesh()).into(),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
+        ..Default::default()
+    });
+}
+
+fn triangle_mesh() -> Mesh {
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    let position = vec![[0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [1.0, 0.0, 0.0]];
+    let normals = vec![[0.0, 0.0, 0.0], [0.0, 0.0, 0.0],[0.0,0.0,1.0]]; // is this just normal vector ?
+    let uvs = vec![[0.0,0.0,0.1],[0.0,0.0,0.0],[0.0,0.0,0.0]]; // wtf is this / some shit about texture coordinates  <== again what kind of shit is this ? 
+
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, position);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+
+    mesh.set_indices(Some(Indices::U32(vec![0, 1, 2])));
+
+    mesh
 }
