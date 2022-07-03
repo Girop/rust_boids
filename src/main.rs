a76a118d3da3f33fb0678eecfe4d6c96a8410c27
unknown <pigor2400@gmail.com>
2022-07-04 01:49:19 +0200

    boid sprite creation system

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,55 @@
-#![allow(unused)]
-use bevy::{
-    prelude::*,
-    render::mesh::{Indices, PrimitiveTopology},
-    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
-};
+#![allow(dead_code)]
+use bevy::{asset, prelude::*, sprite::MaterialMesh2dBundle};
+use rand::Rng;
 
-// const BOID_POPULATION: i32 = 30;
+const BOID_SPRITE: &str = "boid.png";
+const TIME_STEP: f32 = 1./60.;
+
+struct WinSize {
+    w: f32,
+    h: f32,
+}
+
+struct Boid;
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_plugins(DefaultPlugins)
         .insert_resource(WindowDescriptor {
-            title: "Boiiiiids".to_string(),
+            title: "Boids".to_string(),
             ..Default::default()
         })
-        .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_startup_system(spawn_boid)
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    // Create camera
+fn setup(mut commands: Commands, mut windows: ResMut<Windows>) {
+    commands.insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)));
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-}
 
-fn spawn_boid(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn_bundle(MaterialMesh2dBundle {
-        mesh: Mesh2dHandle(meshes.add(triangle_mesh())),
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        ..Default::default()
+    let window = windows.get_primary_mut().unwrap();
+    commands.insert_resource(WinSize {
+        h: window.height(),
+        w: window.width(),
     });
 }
 
-fn triangle_mesh() -> Mesh {
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleStrip);
+fn spawn_boid(mut commands: Commands, windows: Res<WinSize>, assets_server: Res<AssetServer>) {
+    let mut rng = rand::thread_rng();
 
-    let position = vec![[0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [1.0, 0.0, 0.0]];
-    let indices = vec![0, 1, 2];
+    for _i in 0..10 {
+        let pos_x = rng.gen_range(-windows.w..windows.w);
+        let pos_y = rng.gen_range(-windows.h..windows.h);
 
-    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, position);
-    mesh.set_indices(Some(Indices::U32(indices)));
-    mesh
+        commands.spawn_bundle(SpriteBundle {
+            texture: assets_server.load(BOID_SPRITE),
+            transform: Transform {
+                translation: Vec3::new(pos_x, pos_y, 0.0),
+                scale: Vec3::new(0.03, 0.03, 1.),
+                ..Default::default()
+            },
+            ..Default::default()
+        }).insert(Boid);
+    }
 }
-
-fn movement_system() {}
